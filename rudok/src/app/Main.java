c25f0be717d2b7d9729be8128b8dc1084e73bef8
Tadija <tsimic7419rn@raf.rs>
2021-10-23 22:52:49 +0200

    Inicijalni comit, Singleton sablon

---
+++ rudok/src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+import ui.view.MainFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainFrame mainFrame = MainFrame.getInstance();
+        mainFrame.setVisible(true);
+    }
+}

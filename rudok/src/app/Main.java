cb877b8c99015d9810879a8a85b2dcb307bd0a50
Tadija <tsimic7419rn@raf.rs>
2022-01-17 23:50:16 +0100

    Modeli.

--- rudok/src/app/Main.java
+++ rudok/src/app/Main.java
@@ -2,9 +2,16 @@ package app;
 
 import app.view.gui.MainFrame;
 
+
+
 public class Main {
 
     public static void main ( String[] args ) {
         MainFrame.getInstance().init();
+
+
     }
 }
+
+
+
